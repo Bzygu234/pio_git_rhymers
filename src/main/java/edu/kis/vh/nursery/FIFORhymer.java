a9028a114593fd3686680c31bb4a632798475e40
Marcin Gralka <240673@edu.p.lodz.pl>
2023-05-21 22:18:12 +0200

    17. Stworzenie dokumentacji dla klasy FIFORhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,20 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Klasa FIFORhymer rozszerza klasę DefaultCountingOutRhymer i implementuje logikę
+ * wyjmowania elementów z Rhymera w kolejności FIFO
+ *
+ * Zawiera atrybut temp typu DefaultCountingOutRhymer będący tymczasowym pomocniczym Rhymerem
+ *
+ * Zawiera metodę countOut
+ */
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /**
+     * Metoda countOut nie przyjmuje żadnych argumentów oraz zwraca wartość usuniętą z Rhymera w kolejności FIFO
+     */
     @Override
     public int countOut() {
         while (!callCheck())
